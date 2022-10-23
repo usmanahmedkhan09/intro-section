4f65d3c5cc08792d0a149746506e9d81a2ec67d3
usman ahmed khan <usmanahmedkhan@live.com>
2022-10-23 18:22:55 +0500

    added dropdown component

--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -1,3 +1,32 @@
 <template>
-  <div class="dropdown"></div>
+  <div class="dropdown">
+    <div class="dropdown--header">
+      <p>
+        {{ title }}
+      </p>
+      <ArrowDown @click="isOpen = !isOpen" />
+    </div>
+    <div class="dropdown--body" :class="{ open: isOpen }">
+      <ul>
+        <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import ArrowDown from "../assets/svg/arrowdown.vue";
+
+export default defineComponent({
+  components: {
+    ArrowDown,
+  },
+  props: ["title", "list"],
+  setup() {
+    const isOpen = ref(false);
+    return {
+      isOpen,
+    };
+  },
+});
+</script>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="navbar">
     <Logo />
+    <Dropdown :title="'Features'" :list="menu_one" />
+    <Dropdown :title="'Company'" :list="menu_two" />
     <div class="buttons">
       <button>Login</button>
       <button>Register</button>
@@ -8,18 +10,26 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import Logo from "@/assets/svg/logo.vue";
+import Dropdown from "@/components/dropdown.vue";
 
 export default defineComponent({
   components: {
     Logo,
+    Dropdown,
   },
   setup() {
+    const menu_one = ref(["Todo List ", "Calender", "Reminders", "Planning"]);
+
+    const menu_two = ref(["History", "Our Team", "Blog"]);
+
     const getImageUrl = (name: string) => {
       return new URL(`../assets/images/${name}`, import.meta.url).href;
     };
     return {
+      menu_one,
+      menu_two,
       getImageUrl,
     };
   },
